21caa2b3ca92db8279dd7edad108ac5aa9b0ced4
Patryk Korycki <pat.korycki@gmail.com>
2019-10-03 22:49:57 +0200

    Dodane komentarze

--- Rzut_ukosny/mainwindow.cpp
+++ Rzut_ukosny/mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    angle = 0;      //zeruje wszystkie zmienne
+    angle = 0;
     dt = 0;
     V = 0;
     Xn = 0;
@@ -19,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->Graph->addGraph();
     ui->Graph->xAxis->setLabel("x");
     ui->Graph->yAxis->setLabel("y");
+
 }
 
 MainWindow::~MainWindow()
@@ -26,11 +27,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_Generate_clicked()
+void MainWindow::on_pushButton_clicked()
 {
-    QVector<double> x, y;
-    int i = 0;
-
     angle = ui->angle_edit->text().toInt(nullptr, 10);
     dt = ui->dt_edit->text().toDouble(nullptr);
     V = ui->V_edit->text().toDouble(nullptr);
@@ -39,10 +37,21 @@ void MainWindow::on_Generate_clicked()
     Vx = V * cos(angle * M_PI / 180);
     Vy = V * sin(angle * M_PI / 180);
 
-    ui->Graph->xAxis->setRange(0, ui->X_scale->text().toInt(nullptr, 10));
-    ui->Graph->yAxis->setRange(0, ui->Y_scale->text().toInt(nullptr, 10));
+    QVector<double> x, y;
+    int i = 0;
 
-    while(Yn >= 0)
+    if(ui->auto_scale->isChecked())
+    {
+        ui->Graph->xAxis->setRange(0, ((2 * Vy / g) * Vx) * 1.2);
+        ui->Graph->yAxis->setRange(0, ((pow(Vy, 2) / (2 * g)) * 1.2));
+    }
+    else
+    {
+        ui->Graph->xAxis->setRange(0, ui->X_scale->text().toInt(nullptr, 10));
+        ui->Graph->yAxis->setRange(0, ui->Y_scale->text().toInt(nullptr, 10));
+    }
+
+    while((Yn + Vy * dt) > 0)
     {
       x.resize(i + 1);
       y.resize(i + 1);
@@ -54,25 +63,11 @@ void MainWindow::on_Generate_clicked()
       x[i] = Xn;
       y[i] = Yn;
 
-      QCPItemLine *arrowX = new QCPItemLine(ui->Graph);
-      arrowX->start->setCoords(Xn, Yn);
-      arrowX->end->setCoords(Xn + Vx/5, Yn);
-      arrowX->setHead(QCPLineEnding::esSpikeArrow);
-
-      QCPItemLine *arrowY = new QCPItemLine(ui->Graph);
-      arrowY->start->setCoords(Xn, Yn);
-      arrowY->end->setCoords(Xn, Yn + Vy/5);
-      arrowY->setHead(QCPLineEnding::esSpikeArrow);
 
       ui->Graph->graph(0)->setData(x, y);
       ui->Graph->replot();
 
-      ui->V_value->setNum(sqrt(Vx*Vx + Vy*Vy));
-      ui->Vy_value->setNum(Vy);
-      ui->Vx_value->setNum(Vx);
-
-      delete arrowX;
-      delete arrowY;
+      usleep(dt*1 000 000);
       i++;
     }
 }
